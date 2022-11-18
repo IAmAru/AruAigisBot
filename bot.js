3cb2cdab6c677454ade8edc6b38c2efa77a7ed59
Alejandro J <arujimenez98@gmail.com>
2022-11-17 23:58:40 -0600

    updated for v14

--- bot.js
+++ bot.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
-const {Client, RichEmbed} = require('discord.js');
-const Aigis = new Discord.Client();
-const embed = new RichEmbed();
+const PixivApi = require('pixiv-api-client');
+const {Client, GatewayIntentBits, EmbedBuilder, AttachmentBuilder} = require('discord.js')
+const pixiv = new PixivApi();
+const Aigis = new Client({intents: ['Guilds', 'GuildMessages', 'MessageContent']});
+const embed = new EmbedBuilder();
+
 
 Aigis.on('ready', () => {
   console.log(`Logged in as ${Aigis.user.tag}!`);
@@ -11,34 +14,41 @@ Aigis.on('ready', () => {
 const aru = "154078071895359488"
 
 //Message Response to Commands Starts Here
-Aigis.on('message', msg => {
-var input = msg.content.toUpperCase(); 
-if(input == "PING") {
-    msg.reply('Pong!');
+Aigis.on('messageCreate', (message) => {
+var input = message.content.toUpperCase(); 
+
+if(input === "PING") {
+    message.reply('Pong!');
   }
   
 //Hi, Aigis!
 if (input == "HI AIGIS" || input == "HELLO AIGIS" || input == "SUP AIGIS" || input == "HI AIGIS!" || input == "HELLO AIGIS!" || input == "YO AIGIS!") {
-	const Hello = require('./commands/hello.js'); 
-	Hello(input)
-	if(msg.author.id == aru){
-		greeting = `${greeting}, Aru-sama!`
-	} else {
-		greeting = `${greeting}, ${msg.author}!`
+	try {
+		const Hello = require('./commands/hello.js'); 
+		Hello(input)
+		if(message.author.id == aru){
+			greeting = `${greeting}, Aru-sama!`
+		} else {
+			greeting = `${greeting}, ${message.author}!`
+		}
+		message.channel.send(greeting)
 	}
-	msg.channel.send(greeting)
+
+	catch(err) {
+		document.getElementById("demo").innerHTML = err.message;
+	}
 }
 
 //Goodbye, Aigis
 if(input == "BYE AIGIS" || input == "SEE YA AIGIS" || input == "LATER AIGIS" || input == "GOODNIGHT AIGIS" || input == "GOOD NIGHT AIGIS!" || input == "BYE AIGIS!" || input == "SEE YA AIGIS!" || input == "LATER AIGIS!" || input == "GOODNIGHT AIGIS!" || input == "GOOD NIGHT AIGIS!" || input == "BYE, AIGIS" || input == "SEE YA, AIGIS" || input == "LATER, AIGIS" || input == "GOODNIGHT, AIGIS" || input == "GOOD NIGHT, AIGIS" || input == "GOOD NIGHT, AIGIS!" || input == "BYE, AIGIS!" || input == "SEE YA, AIGIS!" || input == "LATER, AIGIS!" || input == "GOODNIGHT, AIGIS!" || input == "GOOD NIGHT, AIGIS!") {
 	const Goodbye = require('./commands/goodbye.js')
 	Goodbye(input)
-	if (msg.author.id==aru){
+	if (message.author.id==aru){
 		farewell = `${farewell}, Aru-sama!`;
 	} else {
-		farewell = `${farewell}, ${msg.author}`;
+		farewell = `${farewell}, ${message.author}`;
 	}
-	msg.channel.send(farewell)
+	message.channel.send(farewell)
 
 }
 
@@ -46,14 +56,14 @@ if(input == "BYE AIGIS" || input == "SEE YA AIGIS" || input == "LATER AIGIS" || 
 if(input == "+WHOISDEBESS"){
 	const Whoisdebess = require('./commands/whoisdebess.js')
 	Whoisdebess(input)
-	msg.channel.send(`${user} ${situation}`);
+	message.channel.send(`${user} ${situation}`);
 }
 
 //money match maker
 if(input == "+MMMAKER"){
 	const MMMaker = require('./commands/mmmaker.js')
 	MMMaker(input);
-	msg.channel.send(`${user1} vs ${user2} ${situation}`);
+	message.channel.send(`${user1} vs ${user2} ${situation}`);
 }
 //Inspirational Quote
 if(input == "+INSPIRATIONALQUOTE"){
@@ -61,28 +71,31 @@ if(input == "+INSPIRATIONALQUOTE"){
 	InspirationalQuote(input);
 	embed.setTitle(embedTitle);
 	embed.setDescription(embedDescription);
-    msg.channel.send(embed);
+    message.channel.send({embeds: [embed]});
 }
 
 // Meme JPG Commands
-if(input == "+BOI" || input == "+DELETTHIS" || input == "+SHOTS" || input == "+DIP" || input == "+B&" || input == "+BAKA" || input == "+HOLLYWOOD" || input == "+NAIROAPEX" || input == "+WHAT"){
+if(input == "+BOI" || input == "+DELETTHIS" || input == "+SHOTS" || input == "+B&" || input == "+BAKA" || input == "+HOLLYWOOD" || input == "+NAIROAPEX" || input == "+WHAT"){
 	const MemeJPG = require('./commands/memejpg.js')
 	MemeJPG(input)
-	msg.channel.sendFile(require('path').join(__dirname, `${memeResult}.jpg`));
+	const attachment = new AttachmentBuilder(require('path').join(__dirname, `${memeResult}.jpg`));
+	message.channel.send({files: [attachment]});
 }
 
 //Meme Png Commands
 if(input == "+ABSOLUTELYSOLID" || input == "+ADDUP" || input == "+ATTENTION" || input == "+MICDROP" || input == "+THEHARDREAD"){
 	const MemeJPG = require('./commands/memejpg.js')
 	MemeJPG(input)
-	msg.channel.sendFile(require('path').join(__dirname, `${memeResult}.png`));	
+	const attachment = new AttachmentBuilder(require('path').join(__dirname, `${memeResult}.png`));
+	message.channel.send({files: [attachment]});	
 }
 
 //Meme Gif Commands
 if(input == "+DIP" || input == "+DONT" || input == "+JUSTDOIT" || input == "+PINGU" || input == "+TECH" || input == "+VIOLIN" || input == "+ILLUMINATI"){
 	const MemeJPG = require('./commands/memejpg.js')
 	MemeJPG(input)
-	msg.channel.sendFile(require('path').join(__dirname, `${memeResult}.gif`));		
+	const attachment = new AttachmentBuilder(require('path').join(__dirname, `${memeResult}.gif`));
+	message.channel.send({files: [attachment]});
 }
 //Dice Function
 if(input.startsWith("+ROLL D")){
@@ -91,16 +104,16 @@ if(input.startsWith("+ROLL D")){
 	embed.setTitle(embedTitle);
 	embed.setColor(embedColor);
 	embed.setDescription(embedDescription);
-	msg.channel.send(embed);
+	message.channel.send({embeds: [embed]});
 }
 //Coinflip Function [Embed]
 if(input.startsWith("+COIN")){
 	const Coin = require('./commands/coin.js'); 
-	Coin(input);
+	Coin(input)
 	embed.setTitle(embedTitle);
 	embed.setColor(embedColor);
 	embed.setDescription(embedDescription);
-	msg.channel.send(embed);
+	message.channel.send({embeds: [embed]});
 	}
 });
 
